7b97d8f1602e80222bc75ae80d82743b4537c662
Maribel <marleyva233@gmail.com>
2018-08-12 20:27:17 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+//my original array
+var myNumArray=[1.1, 3.2, 4.3, 5.4, 6.5];
+
+//empty array
+var roundedArray=[];
+
+
+for (var i=0; i<myNumArray.length; i++){
+	//rounding an array and pushing to another
+	roundedArray.push(Math.round(myNumArray[i]));
+}
+
+
+
+
+
+
+
+//an empty array 
+var randomNumberArray=[];
+
+function randomNumbers(){
+	randomNumberArray=[];
+//pushing random numbers to an array
+	for (var j=0; j<10; j++){
+	randomNumberArray.push(Math.floor(Math.random()*100)+1);
+	}
+	document.getElementById("displayNumbers").innerHTML=randomNumberArray;
+
+}
+
+
+//next excercise is to work with date and time
+function displayDate(){
+	var myDate = new Date();
+
+	var currentSs=myDate.getSeconds();
+	if(currentSs<10){
+		currentSs="0"+currentSs;
+	}
+
+
+	var currentHr=myDate.getHours();
+		currentHr=currentHr-12;
+	if (currentHr<10){
+		currentHr="0"+""+currentHr;
+	}
+	if (currentHr<12){
+		currentSs=currentSs+" pm";
+	}else{
+		currentSs=currentSs+" am";
+	}
+
+
+	var currentMin=myDate.getMinutes();
+	if(currentMin<10){
+		currentMin="0"+""+currentMin;
+	}
+
+
+	var time=currentHr+":"+currentMin+":"+currentSs;
+
+
+	var currentYear=myDate.getFullYear();
+	var currentMonth=myDate.getMonth()+1;
+		if (currentMonth<10){
+			currentMonth="0"+currentMonth;
+		}
+	var currentDay=myDate.getDate();
+
+	var date=currentDay+"/"+currentMonth+"/"+currentYear;
+
+	var displayArea=document.getElementById("displayArea");
+		displayArea.innerHTML=date +" "+ time;
+
+setTimeout(function() {
+    displayDate();
+  }, 1000)
+}
+displayDate();
+
+
+
+
+
+
